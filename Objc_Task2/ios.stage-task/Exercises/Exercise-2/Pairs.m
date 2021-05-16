82201fc5dd94048b40ee5749a305ea4aaeee7cc5
Mikita Abramowicz <gentled.druzhinnik@gmail.com>
2021-05-16 21:53:20 +0300

    feat: implement exercise-2:Pairs

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,19 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    
+    int countOfPairs = 0;
+    int p = 1;
+    
+    for (int i = 0; i < array.count; i++) {
+        for (int k = p; k < array.count; k++) {
+            if (([array[k] intValue] - [array[i] intValue]) == [number intValue]) {
+                countOfPairs++;
+            }
+        }
+        p++;
+    }
+    return countOfPairs;
 }
 
 @end
