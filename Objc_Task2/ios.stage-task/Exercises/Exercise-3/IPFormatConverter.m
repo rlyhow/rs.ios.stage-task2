a101ab31803fbcda186fb93aea532d9fa0b71fb3
Mikita Abramowicz <gentled.druzhinnik@gmail.com>
2021-05-16 23:38:50 +0300

    feat: implement exercise-3:IPFormatConverter

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,37 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    NSMutableString *ipaddress = [[NSMutableString alloc] init];
+    
+    if (numbersArray.count == 0) {
+        return @"";
+    }
+    
+    int count = 0;
+    
+    while (count < 4) {
+        
+        if (count > 0 && count < 4) {
+            [ipaddress appendString:@"."];
+        }
+        
+        if (count < numbersArray.count) {
+            if ([numbersArray[count] intValue] < 0) {
+                return @"Negative numbers are not valid for input.";
+            }
+            if ([numbersArray[count] intValue] > 255) {
+                return @"The numbers in the input array can be in the range from 0 to 255.";
+            }
+            [ipaddress appendString:[numbersArray[count] stringValue]];
+        } else {
+            [ipaddress appendString:@"0"];
+        }
+        
+        count++;
+    }
+    
+    return ipaddress;
 }
 
 @end
