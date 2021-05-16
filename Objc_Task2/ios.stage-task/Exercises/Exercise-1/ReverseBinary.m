74baff7a36b5842639258c4214647ccb3da50150
Mikita Abramowicz <gentled.druzhinnik@gmail.com>
2021-05-16 19:54:45 +0300

    feat: implement exercise-1:ReverseBinary.m

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,15 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 result = 0;
+    UInt8 left = 128;
+    UInt8 right = 128;
+    
+    for (int i = 7; i >= 0; i--) {
+        if (((n << i) & right) == right) {
+            result |= left;
+        }
+        left >>= 1;
+    }
+    return result;
 }
