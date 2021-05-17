ddcbab6037c152984692d744f97c50576c516c9f
Mikita Abramowicz <gentled.druzhinnik@gmail.com>
2021-05-17 14:01:09 +0300

    feat: implement exercise-4:TwoDimensionalArraySorter

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,54 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    // Если массив пустой
+    if (array.count == 0) {
+        return @[];
+    }
+    
+    NSMutableArray *resultArrayOfNumbers = [NSMutableArray new]; // сюда будет записан результат для чисел
+    NSMutableArray *resultArrayOfStrings = [NSMutableArray new]; // сюда будет записан результат для cтрок
+    
+    NSSortDescriptor *sdIncrease = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES]; // сортировка по возрастанию
+    NSSortDescriptor *sdDecrease = [NSSortDescriptor sortDescriptorWithKey:nil ascending:NO]; // сортировка по убыванию
+    
+    NSEnumerator *enumerator = [array objectEnumerator];
+    NSArray *number;
+    
+    while (number = [enumerator nextObject]) {
+        // проверка на двойной массив
+        if (![number isKindOfClass:[NSArray class]]) {
+            return @[];
+        }
+        
+        for (int i = 0; i < number.count; i++) {
+            if ([number[i] isKindOfClass:[NSNumber class]]){
+                [resultArrayOfNumbers addObject:number[i]];
+            } else {
+                [resultArrayOfStrings addObject:number[i]];
+            }
+        }
+    }
+    
+    NSMutableArray *resultEnd = [[NSMutableArray alloc] init];
+    NSArray *resultNumberEnd;
+    NSArray *resultStringEnd;
+    
+    if (resultArrayOfNumbers.count != 0 && resultArrayOfStrings.count == 0) {
+        resultNumberEnd = [resultArrayOfNumbers sortedArrayUsingDescriptors:@[sdIncrease]];
+        return resultNumberEnd;
+    } else if (resultArrayOfNumbers.count == 0 && resultArrayOfStrings.count != 0) {
+        resultStringEnd = [resultArrayOfStrings sortedArrayUsingDescriptors:@[sdIncrease]];
+        return resultStringEnd;
+    } else {
+        resultNumberEnd = [resultArrayOfNumbers sortedArrayUsingDescriptors:@[sdIncrease]];
+        resultStringEnd = [resultArrayOfStrings sortedArrayUsingDescriptors:@[sdDecrease]];
+        [resultEnd addObject:resultNumberEnd];
+        [resultEnd addObject:resultStringEnd];
+    }
+    
+    return resultEnd;
 }
 
 @end
