0fc0446bfe059c5c2b1d2b6b87d77a0f4b91b950
Mikita Abramowicz <gentled.druzhinnik@gmail.com>
2021-05-17 11:12:07 +0300

    feat:Exercise-2 implement sort array

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -7,6 +7,9 @@
     int countOfPairs = 0;
     int p = 1;
     
+    NSSortDescriptor *sd = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES];
+    array = [array sortedArrayUsingDescriptors:@[sd]];
+    
     for (int i = 0; i < array.count; i++) {
         for (int k = p; k < array.count; k++) {
             if (([array[k] intValue] - [array[i] intValue]) == [number intValue]) {
